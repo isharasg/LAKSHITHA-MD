c23853f8316109fcbc044cd517469cd5ad95a192
Lakshitha <nameishara@gmail.com>
2024-10-31 09:28:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,5 +10,5 @@ AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 };
