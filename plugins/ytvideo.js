7ce1284bbf6ef02ae39f6d7e813a03d537cefd7b
Lakshitha <nameishara@gmail.com>
2024-10-31 13:51:50 +0530

    Update ytvideo.js

--- plugins/ytvideo.js
+++ plugins/ytvideo.js
@@ -1,57 +1,88 @@
-
-
 const {cmd , commands} = require('../command')
-const yts = require('yt-search')
-const { fetchJson } = require("../lib/functions")
+const yts = require('yt-search');
+const fg = require('api-dylux');
 
+// -------- Song/Video Download --------
 cmd({
-    pattern: "video",
-    desc: "downlode videos",
-    category: "downlode",
-    react: "🎬",
+    pattern: 'ytdown',
+    alias: ["get"],
+    desc: 'Download Song / Video',
+    use: '.play Title',
+    react: "🗂",
+    category: 'download',
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if(!q) return reply("*Please give me a title*")
-let search = await yts(q)
-let link = search.all[0].url
-let desc = `
-*──────────────────*
-_* 🍁VIDEO DＯＷＮＬＯＤＥＲ By LAKSHITHA-MD🍁 *_
-*──────────────────*
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!q) return reply('Please provide a title.');
 
-🍁 *Title :* ${search.all[0].title}
+        const search = await yts(q);
+        const data = search.videos[0];
+        const url = data.url;
 
-🍁 *Description :* ${search.all[0].description}
+        let desc = `*lakshitha-MD SONG/VIDEO DOWNLOADER . .⚙️*
+        
+⚙️ TITLE - ${data.title}
 
-🍁 *Duration :* ${search.all[0].timestamp}
+⚙️ VIEWS - ${data.views}
 
-🍁 *Ago :* ${search.all[0].ago}
+⚙️ DESCRIPTION - ${data.description}
 
-🍁 *Views :* ${search.all[0].views}
+⚙️ TIME - ${data.timestamp}
 
-🍁 *URL :* ${search.all[0].url}
+⚙️ AGO - ${data.ago}
 
-_📤ᴜᴘʟᴏᴅɪɴɢ ʏᴏᴜʀ ᴀᴜᴅɪᴏ ꜰɪʟʟ📤_
+*Reply This Message With Option*
 
+*1.1 Audio With Normal Format*
+*1.2 Audio With Document Format*
+*2.1 Video With Normal Format*
+*2.2 Video With Document Format*
 
-> LAKSHITHA-MD
-`
+> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ*`;
 
-await conn.sendMessage(from,{image:{url: search.all[0].thumbnail},caption:desc},{quoted:mek})
+        const vv = await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
 
+        conn.ev.on('messages.upsert', async (msgUpdate) => {
+            const msg = msgUpdate.messages[0];
+            if (!msg.message || !msg.message.extendedTextMessage) return;
 
-        let data = await fetchJson (`https://api.dreaded.site/api/ytdl/video?url=${link}`)
+            const selectedOption = msg.message.extendedTextMessage.text.trim();
 
-await conn.sendMessage(from, {
-  video: {url: data.result.downloadLink},
-mimetype: "video/mp4",
- fileName: `${data.result.title}.mp4`,caption: `*🍁© ᴄʀᴇᴀᴛᴇᴅ ʙʏ LAKSHITHA-MD🍁 · · ·* 🧑🏻‍💻`}, { quoted: mek })
+            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === vv.key.id) {
+                switch (selectedOption) {
+                    case '1.1':
+                        let down = await fg.yta(url);
+                        let downloadUrl = down.dl_url;
+                        await conn.sendMessage(from, { audio: { url:downloadUrl }, caption: '> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ*', mimetype: 'audio/mpeg'},{ quoted: mek });
+                        break;
+                    case '1.2':               
+                        // Send Document File
+                        let downdoc = await fg.yta(url);
+                        let downloaddocUrl = downdoc.dl_url;
+                        await conn.sendMessage(from, { document: { url:downloaddocUrl }, caption: '> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ*', mimetype: 'audio/mpeg', fileName:data.title + ".mp3"}, { quoted: mek });
+                        await conn.sendMessage(from, { react: { text: '✅', key: mek.key } })
+                        break;
+                    case '2.1':
+                        let downvid = await fg.ytv(url);
+                        let downloadvUrl = downvid.dl_url;
+                        await conn.sendMessage(from, { video : { url:downloadvUrl }, caption: '> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ*', mimetype: 'video/mp4'},{ quoted: mek });
+                        break;
+                    case '2.2':
+                        let downviddoc = await fg.ytv(url);
+                        let downloadvdocUrl = downviddoc.dl_url;
+                        await conn.sendMessage(from, { document: { url:downloadvdocUrl }, caption: '> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ*', mimetype: 'video/mp4', fileName:data.title + ".mp4" }, { quoted: mek });
+                        break;
+                    default:
+                        reply("Invalid option. Please select a valid option🔴");
+                }
 
-}catch(e){
-    console.log(e)
-    reply(`${e}`)
-}
-}
-)
+            }
+        });
+
+    } catch (e) {
+        console.error(e);
+        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } })
+        reply('An error occurred while processing your request.');
+    }
+});
