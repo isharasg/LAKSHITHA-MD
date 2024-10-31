bf1d87e558d2111b9411303c3cb10d1ab34aa5d4
Lakshitha <nameishara@gmail.com>
2024-10-31 16:35:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "hD0AlbTS#tK4XhTRGiiSLnyEhpt96chhpbtYpzcAMC5swiZOjrP8", //put your session id
+SESSION_ID: process.env.SESSION_ID || "FQRSHAZZ#tT_JKobLwbuBUFQfzh0XjpJg0SSIK-J_1L-XnBRO7R0", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am lakshitha ishara*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
